8b3a4566a85dc2505d29d8c3a223436d88e69c4d
Sune Marcher <sum@miracle.dk>
2016-12-24 16:34:13 +0100

    Fix bad character.

--- src/main/kotlin/nannextract/Main.kt
+++ src/main/kotlin/nannextract/Main.kt
@@ -80,7 +80,7 @@ fun main(args : Array<String>) {
 		val running = api.client.dispatcher().runningCallsCount()
 		val pending = api.client.dispatcher().queuedCallsCount()
 
-§		print("\rRunning: $running, pending: $pending          ")
+		print("\rRunning: $running, pending: $pending          ")
 		if(running == 0 && pending == 0) {
 			break
 		}
